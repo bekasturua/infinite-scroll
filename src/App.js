15f39a051cd2cc59227613783b2c42056d34587c
Beka Sturua <beqasturua3@gmail.com>
2022-07-13 01:11:46 +0400

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import React, { useState } from "react";
+import useBookSearch from "./useBookSearch";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function handleSearch(e) {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+
+  useBookSearch(query, pageNumber);
   return (
     <>
-      <input type="text"></input>
+      <input type="text" onChange={handleSearch}></input>
       <div>Title</div>
       <div>Title</div>
       <div>Title</div>
