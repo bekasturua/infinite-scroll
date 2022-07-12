5211d698632e8b524c61d4eb0ec04f97a41dad0f
Beka Sturua <beqasturua3@gmail.com>
2022-07-13 01:47:54 +0400

    add useEffect

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -8,6 +8,10 @@ export default function useBookSearch(query, pageNumber) {
   const [hasMore, setHasMore] = useState(false);
 
   useEffect(() => {
+    setBooks([]);
+  }, [query]);
+
+  useEffect(() => {
     setLoading(true);
     setError(false);
     let cancel;
